57292412aa865c2e6df7b4c7bc10293141a2d339
szonjaozsvart <szonjaozsvart@gmail.com>
2023-03-16 19:57:23 +0100

    03_16 algos ready

--- baby_algos.js
+++ baby_algos.js
@@ -79,10 +79,10 @@ function highAndLow(numbers) {
         } else {
             positive.push(splitted[i])
         }
-        
+
     }
-    negative.sort((a,b) => (a-b));
-    positive.sort((a,b)=> (a-b));
+    negative.sort((a, b) => (a - b));
+    positive.sort((a, b) => (a - b));
 
     if (negative.length === 0) {
         return `${positive[positive.length-1]} ${positive[0]}`
@@ -94,3 +94,60 @@ function highAndLow(numbers) {
 }
 
 // console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+
+// 4. Descending Order
+
+function descendingOrder(n) {
+    let givenNumbers = n;
+    let splitted = givenNumbers.toString().split('')
+    let sorted = splitted.sort((a, b) => (b - a))
+    return Number(sorted.join(''))
+}
+
+// console.log(descendingOrder(0))
+
+//5. Exes and Ohs
+
+function XO(str) {
+    let xs = [];
+    let os = [];
+    let sizes = str.toLowerCase().split('');
+    let elses = [];
+
+    for (let i = 0; i < sizes.length; i++) {
+        if (sizes[i] === "x") {
+            xs.push(sizes[i])
+        } else if (sizes[i] === 'o') {
+            os.push(sizes[i])
+        } else {
+            elses.push(sizes[i])
+        }
+    }
+    if (xs.length === 0 && os.length === 0) {
+        return true
+    } else if (xs.length === os.length) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+// console.log(XO("zzoo"))
+
+// 6. Shortest Word
+
+function findShort(s) {
+    let words = s.split(' ');
+    let lengths = [];
+
+    for (let i = 0; i < words.length; i++) {
+        let letters = 0;
+        letters = words[i].length;
+        lengths.push(letters)
+    }
+    lengths.sort((a, b) => a - b)
+    return lengths[0]
+}
+
+console.log(findShort("Let's travel abroad shall we"));

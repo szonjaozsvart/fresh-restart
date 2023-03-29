51e1508baebdf6153a77ebc88c7eb36077d8260d
szonjaozsvart <szonjaozsvart@gmail.com>
2023-03-29 20:07:31 +0200

    today's algo

--- level_up_algos.js
+++ level_up_algos.js
@@ -17,4 +17,24 @@ function createPhoneNumber(numbers) {
     return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
 }
 
-console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+// console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+
+//3. Sum of Digits
+
+function digitalRoot(n) {
+    let givenNumber = n;
+    let splitted = givenNumber.toString();
+    let sum = 0;
+    for (let j = 0; j < splitted.length; j++) {
+        sum += Number(splitted[j])
+    }
+    if (splitted.length > 1) {
+        return digitalRoot(sum) // If the splitted has more than one digit, recursively call the digitalRoot 
+                                // function with sum as the argument and return its result. This will 
+                                // continue the process of summing the digits until a single-digit number is produced.
+    }
+    return sum
+}
+
+console.log(digitalRoot(16)) //7
+console.log(digitalRoot(456)) //6
